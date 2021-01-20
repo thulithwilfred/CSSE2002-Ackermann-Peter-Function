f6c4fdd56ef2f05f6e2a5fc4928a2cf3fda404b8
Thulith W Mallawa <thulith.mallawa@uqconnect.edu.au>
2021-01-20 23:20:50 +1000

    /* Complete */ Logs: StackOverflow due to large amounts of recursion on big numbers. Works as intended on smaller numbers.
    Potential Fix: Increase stack size, but the excessive recursion is bound to cause stack overflow at some point (?).

---
+++ src/com/UQ/Main.java
@@ -0,0 +1,68 @@
+package com.UQ;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner inputMN = new Scanner(System.in);
+        String [] functionInputs  = new String[4];
+        short [] parsedMN = new short[2];
+
+        System.out.println("Enter Ackermann function inputs");
+
+        while (true) {
+
+            if (parseInputs(inputMN, functionInputs, parsedMN)) {
+                long ackVal = ackermann(parsedMN[0], parsedMN[1]);
+                System.out.println("Ackermann Value: " + ackVal);
+            }
+
+        }
+
+    }
+
+    public static long ackermann(short m, short n) {
+
+        if (m == 0) {
+            return (long)n+1;
+        } else if (m > 0 && n == 0) {
+
+            return ackermann((short)(m - 1), (short)1);
+
+        } else if (m > 0 && n > 0) {
+            return ackermann((short)(m- 1), (short)ackermann(m, (short)(n - 1)));
+        }
+        return 0;
+    }
+
+    public static boolean parseInputs(Scanner inputMN, String[] functionInputs, short[] parsedMN) {
+
+        if (inputMN.hasNextLine()) {
+
+            String inputs = inputMN.nextLine();
+
+            functionInputs = inputs.split(",");
+
+            try {
+                parsedMN[0] = Short.parseShort(functionInputs[0]);
+                parsedMN[1] = Short.parseShort(functionInputs[1]);
+
+                if (parsedMN[0] >= 0 && parsedMN[1] >= 0) {
+                    return true;
+                } else {
+                    System.out.println("Usage: m,n : Positive integers only");
+                }
+
+            } catch (NumberFormatException ex) {
+                System.out.println("Usage: m,n : Positive integers only");
+            }
+
+        }
+        return false;
+    }
+
+
+}
